8a5f12e993a881af9e952bc6bdd95811af17a823
Luca <lucajoaquinnsn@gmail.com>
2026-04-11 19:16:49 -0300

    Termindo Usuarios con permisos (vista)

--- database/migrations/2026_04_08_232800_create_productos_table.php
+++ database/migrations/2026_04_08_232800_create_productos_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->timestamps(); //Alta
+            //Tendria que haber fecha de baja/modificación
 
             $table->string('nombre');
             $table->text('descripcion');
