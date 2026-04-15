48360090939e38df2168f2e22e317e8dc699c17f
LucaJSN <lucajoaquinnsn@gmail.com>
2026-04-15 20:20:52 -0300

    Avance con la vista de comercialización, incluye arreglo de conflictos de versiones en boostrap

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
@@ -18,15 +18,8 @@
                     <li class="nav-item">
                         <a class="nav-link" href="/catalogo">Catalogo</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Productos
-                        </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/comercializacion">Comercialización</a>
                     </li>
                     @auth
                     <!-- Solo se verá el link (btn admin) a esta vista para los administradores -->
