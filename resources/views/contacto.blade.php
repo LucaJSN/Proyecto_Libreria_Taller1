7a29c51efac8a27a0c7b1c7afc1305bbcf0617bb
LucaJSN <lucajoaquinnsn@gmail.com>
2026-04-22 20:13:39 -0300

    Modificación vista contacto

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,34 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="container" id="contacto">
-            <div class="row">
-                <div class="col-6">
-                    <div>
-                        <label for="exampleFormControlInput1" class="form-label">Nombre y apellido</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nombre">
-                    </div>
-                    <div>
-                        <label for="exampleFormControlInput1" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-                    </div>
-                    <div>
-                        <label for="exampleFormControlInput1" class="form-label">Telefono</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="+00 000 000000">
-                    </div>
-                    <div>
-                        <label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+<div class="contacto-container">
+    <div class="container py-5 contacto-container">
+        <div class="contacto-wrapper">
+            <div class="contacto-header">
+                <h1>Contacto</h1>
+                <p>Estamos aquí para ayudarte. Ponete en contacto con nosotros.</p>
+            </div>
+
+            <div class="row g-4">
+                <div class="col-lg-8">
+                    <h2>Donde encontrarnos</h2>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3540.1055964934553!2d-58.7851455246145!3d-27.46597171654735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94456ae1f9f545a7%3A0x4480e6b34abd15ce!2sCampus%20Unne!5e0!3m2!1ses-419!2sar!4v1776297058801!5m2!1ses-419!2sar" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="info-card">
+                        <h3>Nuestras Redes</h3>
+                        <p>Seguinos para enterarte de todas las novedades.</p>
+                        
+                        <div class="redes-lista">
+                            <li>
+                                <a href="#" class="red-link instagram">
+                                <i class="fab fa-instagram"></i> Instagram
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="red-link facebook">
+                                <i class="fab fa-facebook-f"></i> Facebook
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="red-link whatsapp">
+                                <i class="fab fa-whatsapp"></i> WhatsApp
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="red-link tiktok">
+                                <i class="fab fa-tiktok"></i> TikTok
+                                </a>
+                            </li>
+                        </div>
+
+                        <hr>
+
+                        <div class="contacto-directo">
+                            <p><i class="fas fa-map-marker-alt"></i> Campus Deodoro Roca</p>
+                            <p><i class="fas fa-envelope"></i> contacto@puntoybarra.com</p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-6">
-                    <h3>Contactanos</h3>
-                        <p>bautiovallebravo@gmail.com</p>
-                        <p>+54 000 00000</p>
-                        <p>379-00-00000</p>
-                        <p>9 de Julio - Corrientes</p>
-                </div>
-            </div>        
+            </div>
         </div>
     </div>
 </div>
