e6f2c394211ee5c6938e4bc66d0ca330af5c1b10
LucaJSN <lucajoaquinnsn@gmail.com>
2026-06-10 20:50:19 -0300

    progreso en ventas, bastantes problemas, hay que revisar el registro de usuarios

---
+++ database/migrations/2026_06_10_233616_create_ventas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('id_usuario')
+                  ->constrained('usuarios')
+                  ->cascadeOnUpdate()
+                  ->cascadeOnDelete();
+
+            $table->dateTime('fecha_venta');
+
+            $table->decimal('total', 10, 2);
+
+            $table->string('estado')
+                  ->default('Pendiente');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('ventas');
+    }
+};
---
+++ database/migrations/2026_06_10_233648_create_detalle_ventas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('detalle_ventas', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('id_venta')
+                  ->constrained('ventas')
+                  ->cascadeOnUpdate()
+                  ->cascadeOnDelete();
+
+            $table->foreignId('id_producto')
+                  ->constrained('productos')
+                  ->cascadeOnUpdate()
+                  ->cascadeOnDelete();
+
+            $table->integer('cantidad');
+
+            $table->decimal('precio_unitario', 10, 2);
+
+            $table->decimal('subtotal', 10, 2);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('detalle_ventas');
+    }
+};
+
